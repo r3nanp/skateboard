11ff9162e998dfcddd079979a7b4ac2e0e901cc3
Renan Pereira <renan.souza.dev@gmail.com>
2022-04-10 00:12:01 -0300

    feat: add translateX animation

--- src/screens/HomePage/HomePage.screen.tsx
+++ src/screens/HomePage/HomePage.screen.tsx
@@ -1,15 +1,42 @@
-import { ScrollView, Text, View } from 'react-native'
+import Animated, {
+  useAnimatedScrollHandler,
+  useAnimatedStyle,
+  useSharedValue,
+} from 'react-native-reanimated'
 
 import { Skate } from '../../components'
 import { SKATES } from '../../constants'
 import { styles } from './homePage.styles'
 
 export const HomePage = () => {
+  const translateX = useSharedValue(0)
+
+  const scrollHandler = useAnimatedScrollHandler({
+    onScroll: event => {
+      translateX.value = event.contentOffset.x
+    },
+  })
+
+  const scrollTranslation = useAnimatedStyle(() => ({
+    transform: [
+      {
+        translateX: translateX.value,
+      },
+    ],
+  }))
+
   return (
-    <ScrollView style={styles.container} horizontal>
+    <Animated.ScrollView
+      horizontal
+      pagingEnabled
+      onScroll={scrollHandler}
+      scrollEventThrottle={16}
+      showsHorizontalScrollIndicator={false}
+      style={[styles.container, scrollTranslation]}
+    >
       {SKATES.map(skate => {
         return <Skate key={skate.id} skate={skate} />
       })}
-    </ScrollView>
+    </Animated.ScrollView>
   )
 }
