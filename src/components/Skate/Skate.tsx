a81aca05cc0b077ed73a03370395700921aae6d5
Renan Pereira <renan.souza.dev@gmail.com>
2022-04-10 00:11:41 -0300

    feat: add skate styles

--- src/components/Skate/Skate.tsx
+++ src/components/Skate/Skate.tsx
@@ -1,8 +1,23 @@
-import { View } from 'react-native'
+import { Image, Text, View } from 'react-native'
 
 import { SkateProps } from './skate.types'
 import { styles } from './skate.styles'
 
 export const Skate = ({ skate }: SkateProps) => {
-  return <View style={styles.container}></View>
+  return (
+    <View style={styles.container}>
+      <View style={styles.circleContainer}>
+        <View style={styles.circle} />
+
+        <Image
+          source={skate.source}
+          style={styles.image}
+          resizeMode="contain"
+        />
+      </View>
+
+      <Text style={styles.title}>{skate.title}</Text>
+      <Text style={styles.description}>{skate.description}</Text>
+    </View>
+  )
 }
--- src/components/Skate/skate.styles.ts
+++ src/components/Skate/skate.styles.ts
@@ -3,9 +3,43 @@ import { Dimensions, StyleSheet } from 'react-native'
 export const { width: PAGE_WIDTH, height: PAGE_HEIGHT } =
   Dimensions.get('window')
 
+const CIRCLE_WIDTH = PAGE_WIDTH * 0.7
+
 export const styles = StyleSheet.create({
   container: {
     width: PAGE_WIDTH,
     height: PAGE_HEIGHT,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingHorizontal: 50,
+  },
+  circleContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    aspectRatio: 1,
+    width: CIRCLE_WIDTH,
+    marginBottom: 120,
+  },
+  image: {
+    aspectRatio: 1,
+    position: 'absolute',
+    height: PAGE_HEIGHT * 0.5,
+  },
+  circle: {
+    width: '100%',
+    height: '100%',
+    backgroundColor: 'white',
+    borderRadius: CIRCLE_WIDTH / 2,
+  },
+  title: {
+    fontWeight: '700',
+    fontSize: 35,
+    textAlign: 'center',
+    marginBottom: 15,
+  },
+  description: {
+    fontSize: 14,
+    color: 'gray',
+    textAlign: 'center',
   },
 })
